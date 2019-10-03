f9990e530a749092128cb8e47c9d5a74c1e2bcb6
Spriha <spriha@mindfiresolutions.com>
2019-10-04 00:26:36 +0530

    worked on raising and handling events and updating state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        value: this.props.value
+        value: this.props.counter.value
     };
 
     handleIncrement = product => {
@@ -20,6 +20,7 @@ class Counter extends Component {
                 {this.props.children}
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={() => this.handleIncrement({ id: 1 })} className="btn btn-secondary btm-sm">Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,10 +9,17 @@ class Counters extends Component {
             { id: 3, value: 0 },
             { id: 4, value: 0 },
         ]
-     }
+    };
+
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters: counters });
+        // this.setState({ counters });
+    }
+
     render() { 
         return ( <div>
-            {this.state.counters.map(counter => <Counter key={counter.id} value={counter.value}>
+            {this.state.counters.map(counter => <Counter key={counter.id} onDelete={this.handleDelete} counter={counter}>
                 <h4>Title</h4>
             </Counter>)}
         </div> );
