050d28bc03353620374cb8804e29a790dfadfe1e
Spriha <spriha@mindfiresolutions.com>
2019-10-03 16:33:38 +0530

    worked on sending arguments to event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,24 +5,20 @@ class Counter extends Component {
         count: 0
     };
 
-    // constructor() {
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
-
-    // handleIncrement() {
-    //     console.log(this);
-    // }
-
-    handleIncrement = () => {
-        console.log(this);
+    handleIncrement = product => {
+        console.log(product);
+        this.setState({ count: this.state.count + 1 });
     }
+
+    // doHandleIncrement = () => {
+    //     this.handleIncrement({ id: 1 });
+    // }
 
     render() { 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btm-sm">Increment</button>
+                <button onClick={() => this.handleIncrement({ id: 1 })} className="btn btn-secondary btm-sm">Increment</button>
             </div>
         );
     }
