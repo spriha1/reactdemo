7423764bc50c7b45fe167a782f5a5a4de2cd753e
Spriha <spriha@mindfiresolutions.com>
2019-10-03 18:07:17 +0530

    worked on composing components and passing data and children to components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        value: this.props.value
     };
 
     handleIncrement = product => {
         console.log(product);
-        this.setState({ count: this.state.count + 1 });
+        this.setState({ value: this.state.value + 1 });
     }
 
     // doHandleIncrement = () => {
@@ -17,6 +17,7 @@ class Counter extends Component {
     render() { 
         return (
             <div>
+                {this.props.children}
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={() => this.handleIncrement({ id: 1 })} className="btn btn-secondary btm-sm">Increment</button>
             </div>
@@ -25,12 +26,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? "Zero" : count;
     }
 }
