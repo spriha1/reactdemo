2996caee77b27eb0be5d25c8b201c18e6254380f
Spriha <spriha@mindfiresolutions.com>
2019-10-04 13:04:32 +0530

    worked on syncing multiple components and lifting state up

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,43 +2,12 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            { id: 1, value: 4 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
-        ]
-    };
-
-    handleIncrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = { ...counter };
-        counters[index].value++;
-        this.setState({ counters });
-    };
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({ counters });
-    };
-
-    handleDelete = counterId => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({ counters: counters });
-        // this.setState({ counters });
-    }
 
     render() { 
         return ( 
         <div>
-            <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
-
-            {this.state.counters.map(counter => <Counter key={counter.id} onIncrement={this.handleIncrement} onDelete={this.handleDelete} counter={counter}>
+            <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+            {this.props.counters.map(counter => <Counter key={counter.id} onIncrement={this.props.onIncrement} onDelete={this.props.onDelete} counter={counter}>
                 <h4>Title</h4>
             </Counter>)}
         </div> );
